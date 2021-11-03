00514f2198a95ad46d36b073bbcb5e7ad9ac15ae
yashtiwari1906 <68691750+yashtiwari1906@users.noreply.github.com>
2021-11-03 20:30:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     ap.add_argument("-c", "--config", required=True, help="Path to configuration file")
     ap.add_argument("-d", "--device", default="cpu", type=str, help="Whether to perform task on CPU ('cpu') or GPU ('cuda')")
     ap.add_argument("-t", "--task", default="train", type=str, help="Task to perform. Choose between ['train', 'test']")
-    ap.add_argument("-p", "--path", default="train", type=str, help="path to test file in csv")
+    ap.add_argument("-p", "--path", default=None, type=str, help=  "path to test file in csv")
     ap.add_argument("-o", "--output_dir", required = True, type=str, help="Output directory path")
     ap.add_argument("-l", "--load", default=None, type=str, help="Path to directory containing models and meta_data")
     ap.add_argument("-f", "--file", default="", type=str, help="Path to single testing file")
@@ -36,8 +36,8 @@ if __name__ == "__main__":
     
     elif args["task"] == "test":
         assert args["output_dir"] is not None, "Please provide a checkpoint to load using --load to check test performance"
-        test = TrainModel(args["path"])
-        test.get_test_performance()
+        test = TrainModel(args)
+        test.get_test_performance(args["path"])
 
 
     elif args["task"] == "single_test":
